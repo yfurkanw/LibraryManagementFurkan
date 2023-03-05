2755e9c9977853a4571837eeb82df5b2c2a8677e
Furkan Ekmekçibaşı <yfurkanw@gmail.com>
2023-03-05 22:30:49 +0300

    fix: class Book and  class Library  was added

---
+++ main.py
@@ -0,0 +1,30 @@
+
+import sqlite3
+class Library:
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.c = self.conn.cursor()
+
+    def add_book(self, book):
+        self.c.execute('INSERT INTO books (title, author, year) VALUES (?, ?, ?)', (book.title, book.author, book.year))
+        self.conn.commit()
+
+    def remove_book(self, book):
+        self.c.execute('DELETE FROM books WHERE title=? AND author=? AND year=?', (book.title, book.author, book.year))
+        self.conn.commit()
+
+    def update_book(self, old_book, new_book):
+        self.c.execute('UPDATE books SET title=?, author=?, year=? WHERE title=? AND author=? AND year=?', 
+                        (new_book.title, new_book.author, new_book.year, old_book.title, old_book.author, old_book.year))
+        self.conn.commit()
+
+    def get_books(self):
+        self.c.execute('SELECT * FROM books')
+        books = []
+        for row in self.c.fetchall():
+            book = Book(row[1], row[2], row[3])
+            books.append(book)
+        return books
+
+    def __del__(self):
+        self.conn.close()
